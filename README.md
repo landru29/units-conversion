c7316708b6f3cb0264b848677c95b8243cf4ea15
Cyrille M <cmeichel@free.fr>
2016-01-03 12:17:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# beer-toolbox
+
+Angular toolbox for converting from a unit to another.
+
+## Usage
+
+Include ``unitsConversion`` deps in you AngularJs application
---
+++ docs/js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "Library"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "UnitsConversion.Polynome",
+      "shortName": "UnitsConversion.Polynome",
+      "type": "service",
+      "moduleName": "UnitsConversion",
+      "shortDescription": "Perform basic operations on a polynome, including solve",
+      "keywords": "a0 a1 a2 a3 api basic direct equation including inject invert method operations pass perform polynome polynomeobj service set solution solutions solve solveeq unitsconversion"
+    },
+    {
+      "section": "api",
+      "id": "UnitsConversion.UnitsConversion",
+      "shortName": "UnitsConversion.UnitsConversion",
+      "type": "service",
+      "moduleName": "UnitsConversion",
+      "shortDescription": "Units conversions",
+      "keywords": "api array coeficients conversions convert converted convertion current decode decoded decodetype description destination fromto getphysicalunits list mass method optional options polynome polynomecoef precision register registerconversion service type unit units unitsconversion unittype"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
